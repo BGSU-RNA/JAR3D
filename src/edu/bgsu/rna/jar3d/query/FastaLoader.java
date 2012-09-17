977714511774921b292f33f69097679dbd905d67
Blake Sweeney <blakes.85@gmail.com>
2012-09-17 16:08:19 -0400

    Remove unused imports

--- src/edu/bgsu/rna/jar3d/query/FastaLoader.java
+++ src/edu/bgsu/rna/jar3d/query/FastaLoader.java
@@ -1,8 +1,6 @@
 package edu.bgsu.rna.jar3d.query;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
