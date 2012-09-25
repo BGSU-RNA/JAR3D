fa0db0107bb82c15196a65ca874479b08cdfa371
Blake Sweeney <blakes.85@gmail.com>
2012-09-25 12:05:43 -0400

    Import only what is used

--- src/edu/bgsu/rna/jar3d/Alignment.java
+++ src/edu/bgsu/rna/jar3d/Alignment.java
@@ -1,7 +1,15 @@
 package edu.bgsu.rna.jar3d;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Formatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.Vector;
 
 import edu.bgsu.rna.jar3d.query.Query;
 import edu.bgsu.rna.jar3d.results.ImmutableLoopResult;
