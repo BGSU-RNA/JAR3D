ad6fa624ab68f75bd8c6e76bad189b9f80a88fcd
Blake Sweeney <blakes.85@gmail.com>
2012-11-06 14:43:20 -0500

    Document SequenceResult interface.

--- src/main/java/edu/bgsu/rna/jar3d/results/SequenceResult.java
+++ src/main/java/edu/bgsu/rna/jar3d/results/SequenceResult.java
@@ -47,19 +47,59 @@ public interface SequenceResult {
      */
 	public String queryId();
 
+    /**
+     * Get the sequence id of the sequence this belongs to.
+     *
+     * @return The sequence id.
+     */
 	public String sequenceId();
 
+    /**
+     * Get the motif Id of the motif that this was scored against.
+     *
+     * @return The motif id.
+     */
 	public String motifId();
 
+    /**
+     * Get the score of this result.
+     *
+     * @return The score.
+     */
 	public double score();
 
+    /**
+     * Get the percentile of this result.
+     *
+     * @return The percentile.
+     */
 	public double percentile();
 
+    /**
+     * Get the edit distance of this result.
+     *
+     * @return The edit distance.
+     */
 	public int editDistance();
 
+    /**
+     * Return true if this sequence was run in a rotated orientation.
+     *
+     * @return true if rotated.
+     */
 	public boolean isRotated();
 
+    /**
+     * Set the loop result this belongs to.
+     *
+     * @param result The LoopResult.
+     */
 	public void setLoopResult(LoopResult result);
 
+    /**
+     * Get the Loop result this belongs to.
+     *
+     * @return The loop result.
+     */
 	public LoopResult getLoopResult();
 }
