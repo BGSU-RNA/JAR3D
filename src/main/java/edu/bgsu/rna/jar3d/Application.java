cd9071e3216e8fd74ae4721a8f77799148017004
Craig L. Zirbel <zirbel@bgsu.edu>
2013-11-05 15:16:57 +0100

    Put a more sensible default range limit

--- src/main/java/edu/bgsu/rna/jar3d/Application.java
+++ src/main/java/edu/bgsu/rna/jar3d/Application.java
@@ -22,9 +22,9 @@ import edu.bgsu.rna.jar3d.results.LoopResult;
  */
 public class Application {
 
-	/** Default range limit */
+	/** Default range limit for how far to look left and right in the sequence, mostly for alignments */
 	// 2013-11-05 CLZ The user needs a way to override the default range limit
-	public static final int DEFAULT_RANGE_LIMIT = 9999999;
+	public static final int DEFAULT_RANGE_LIMIT = 100;
 
 	/** Default model type */
 	// 2013-11-05 CLZ This should no longer be used.  No default.
