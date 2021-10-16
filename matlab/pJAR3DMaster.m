cc3b3307004e90d47c2d784f20fa2d3c6505b988
Craig L. Zirbel <zirbel@bgsu.edu>
2021-10-15 21:43:46 -0400

    Reorder stages

--- matlab/pJAR3DMaster.m
+++ matlab/pJAR3DMaster.m
@@ -2,30 +2,43 @@
 
 % -------------------------------------------------------------------- % choices for the user
 
-Release = ['HL' filesep '1.17'];
+Input = ['HL_3.50_2021-10-13_17'];
+Release = ['HL' filesep '3.50'];
 
-MotifLibraryLocation = 'C:\Users\zirbel\Documents\JAR3DMotifs\';
-OutputBase = pwd;                                                      % start Matlab in the directory containing IL and HL
+Input = ['IL_3.50_2021-10-13_14'];
+Release = ['IL' filesep '3.50'];
+
+MotifLibraryLocation = 'C:\Users\zirbel\Documents\JAR3D\Motifs\';      % where to read results of clustering
+OutputBase = 'C:\Users\zirbel\Documents\JAR3D\';                       % where to write JAR3D models
+MatlabCodepath = 'C:\Users\zirbel\Documents\GitHub\JAR3D\Matlab';      % location of Matlab code
 JAR3Dpath = 'C:\Users\zirbel\Documents\GitHub\JAR3D\target\classes';   % location of class files for Java programs; needed for scoring sequences
 Pythonpath = 'C:\Users\zirbel\Documents\GitHub\JAR3D\python';          % location of python programs in the JAR3D release
 
 % -------------------------------------------------------------------- % run JAR3D on the selected motif group
 
-MotifLibraryPath = [MotifLibraryLocation filesep Release filesep 'motifs'];
+addpath(MatlabCodepath)
 javaaddpath(JAR3Dpath)
 
+MotifLibraryPath = [MotifLibraryLocation Input filesep 'mat'];
+
+diary([OutputBase Release filesep 'JAR3D log' strrep(char(datetime),':','-') '.txt'])
+
 pMakeSCFGModels(MotifLibraryPath,OutputBase,Release,1)
+
+pGenerateRandomMotifSequences(OutputBase,Release,3)    % document this
+pJAR3DFalsePositiveStudy(OutputBase,Release,3)  % 3 means to use model-specific cutoffs
+pSetModelSpecificCutoffs(OutputBase,Release,0)
+
+pGenerateRandomMotifSequences(OutputBase,Release,1)    % document this
+pJAR3DFalsePositiveStudy(OutputBase,Release,1)  % 1 means to read FASTA files and parse and calculate edit distance; slow
+
+pGenerateRandomMotifSequences(OutputBase,Release,2)    % document this
+pJAR3DFalsePositiveStudy(OutputBase,Release,2)  % 2 means to accumulate false positive data, which takes some RAM
+
+pJAR3DDiagnostics(OutputBase,Release,Release,1)
+
 try
-	system(['python ' Pythonpath filesep 'GroupToModelDiagnostic.py ' OutputBase filesep Release]);
+	system(['python ' Pythonpath filesep 'GroupToModelDiagnostic.py ' OutputBase Release]);
 catch
 	fprintf('Not able to run the diagnostic in which sequences from 3D structures are aligned to their JAR3D models\n');
 end
-pGenerateRandomMotifSequences(OutputBase,Release)
-
-pJAR3DFalsePositiveStudy(OutputBase,Release,1)                         % 1 means to read FASTA files and parse and calculate edit distance; slow
-pJAR3DFalsePositiveStudy(OutputBase,Release,2)                         % 2 means to accumulate false positive data, which takes some RAM
-
-pSetModelSpecificCutoffs(OutputBase,Release,0)
-pJAR3DFalsePositiveStudy(OutputBase,Release,3)                         % 3 means to use model-specific cutoffs
-
-pJAR3DDiagnostics(OutputBase,Release,Release,1)
